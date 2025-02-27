9305be156d61f0ebc7a20dcc64b559e2a9c56465
eilon <3eilon@gmail.com>
2025-02-27 03:43:10 -0700

    add a copy of the songs for new album

--- LA1/src/model/Album.java
+++ LA1/src/model/Album.java
@@ -1,23 +1,28 @@
 package model;
+
 import java.util.List;
 import java.util.ArrayList;
 
 public class Album {
-	private final String title;
-	private final String artist;
-	private final String genre;
-	private final String year;
-	private final List<Song> songs;
-	
-	public Album(String t, String a, String g, String y, ArrayList<Song> s) {
-		title = t;
-		artist = a;
-		genre = g;
-		year = y;
-		songs = s;
-	}
+    private final String title;
+    private final String artist;
+    private final String genre;
+    private final String year;
+    private final List<Song> songs;
 
-	public String toString() {
-		return this.title + "," + this.artist + "," + this.genre + "," + this.year + "," + songs.size();
-	}
+    public Album(String t, String a, String g, String y, ArrayList<Song> s) {
+        title = t;
+        artist = a;
+        genre = g;
+        year = y;
+        songs = new ArrayList<>(s);
+    }
+
+    public List<Song> getSongs() {
+		return new ArrayList<>(songs);
+    }
+
+    public String toString() {
+        return this.title + "," + this.artist + "," + this.genre + "," + this.year + "," + songs.size();
+    }
 }
