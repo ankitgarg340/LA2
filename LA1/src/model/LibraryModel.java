976e69fe464da3d9b9d2cc2316e3216e279ba8d5
eilon <3eilon@gmail.com>
2025-02-27 21:01:55 -0700

    add functions to LibraryModel

--- LA1/src/model/LibraryModel.java
+++ LA1/src/model/LibraryModel.java
@@ -21,12 +21,7 @@ public class LibraryModel {
     }
 
     public boolean containSong(Song song) {
-        for (SongInLibrary sil : songs) {
-            if (sil.getSong().getTitle().equals(song.getTitle())) {
-                return true;
-            }
-        }
-        return false;
+        return getSongInLibraryFromSong(song) != null;
     }
 
     public void addAlbum(Album album) {
@@ -39,6 +34,154 @@ public class LibraryModel {
     }
 
     public void createPlaylist(String name) {
-        playlists.add(new Playlist(name));
+        if (!getAllPlaylistsNames().contains(name)) {
+            playlists.add(new Playlist(name));
+        }
+    }
+
+    public List<Song> getAllSongs() {
+        List<Song> returnSongs = new ArrayList<>();
+        for (SongInLibrary s : songs) {
+            returnSongs.add(s.getSong());
+        }
+        return returnSongs;
+    }
+
+    public List<Album> getAllAlbums() {
+        return new ArrayList<>(albums);
+    }
+
+    public List<String> getAllPlaylistsNames() {
+        List<String> returnPlaylistsNames = new ArrayList<>();
+        for (Playlist p : playlists) {
+            returnPlaylistsNames.add(p.getName());
+        }
+        return returnPlaylistsNames;
+    }
+
+    public void rateSong(Song s, int rating) throws IllegalAccessException {
+        SongInLibrary sil = getSongInLibraryFromSong(s);
+        if (sil != null) {
+            if (rating < 1 || rating > 5) {
+                throw new IllegalAccessException("bad rating");
+            }
+            sil.rate(rating);
+
+            if (rating == 5) {
+                sil.markFavorite();
+            }
+        }
+    }
+
+    public void markSongFavorite(Song s) {
+        SongInLibrary sil = getSongInLibraryFromSong(s);
+        if (sil != null) {
+            sil.markFavorite();
+        }
+    }
+
+    public void markSongUnFavorite(Song s) {
+        SongInLibrary sil = getSongInLibraryFromSong(s);
+        if (sil != null) {
+            sil.markUnFavorite();
+        }
+    }
+
+    public Playlist getPlaylistFromName(String name) {
+        for (Playlist p : playlists) {
+            if (p.getName().equals(name)) {
+                return p;
+            }
+        }
+        return null;
+    }
+
+    public List<Song> getSongsOfPlaylist(String playlistName) {
+        return getPlaylistFromName(playlistName).getSongs();
+    }
+
+    public void addSongToPlayList(String playlistName, Song song) {
+        if (containSong(song)) {
+            Playlist playlist = getPlaylistFromName(playlistName);
+            playlist.addSong(song);
+        }
+    }
+
+    public void removeSongToPlayList(String playlistName, Song song) {
+        if (containSong(song)) {
+            Playlist playlist = getPlaylistFromName(playlistName);
+            playlist.removeSong(song);
+        }
+    }
+
+    public List<Song> getSongsByTitle(String title) {
+        ArrayList<Song> returnList = new ArrayList<Song>();
+
+        for (SongInLibrary sil : songs) {
+            if (sil.getSong().getTitle().equals(title)) {
+                returnList.add(sil.getSong());
+            }
+        }
+
+        return returnList;
+    }
+
+    public List<Song> getSongsByArtist(String artist) {
+        ArrayList<Song> returnList = new ArrayList<Song>();
+
+        for (SongInLibrary sil : songs) {
+            if (sil.getSong().getArtist().equals(artist)) {
+                returnList.add(sil.getSong());
+            }
+        }
+
+        return returnList;
+    }
+
+    public List<Song> getFavoriteSongs() {
+        ArrayList<Song> returnList = new ArrayList<Song>();
+
+        for (SongInLibrary sil : songs) {
+            if (sil.isFavorite()) {
+                returnList.add(sil.getSong());
+            }
+        }
+
+        return returnList;
+    }
+
+
+    public List<Album> getAlbumsByTitle(String album) {
+        ArrayList<Album> returnList = new ArrayList<Album>();
+
+        for (Album a : albums) {
+            if (a.getTitle().equals(album)) {
+                returnList.add(a);
+            }
+        }
+
+        return returnList;
+    }
+
+    public List<Album> getAlbumsByArtist(String artist) {
+        ArrayList<Album> returnList = new ArrayList<Album>();
+
+        for (Album a : albums) {
+            if (a.getArtist().equals(artist)) {
+                returnList.add(a);
+            }
+        }
+
+        return returnList;
+    }
+
+
+    private SongInLibrary getSongInLibraryFromSong(Song s) {
+        for (SongInLibrary sil : songs) {
+            if (sil.getSong().equals(s)) {
+                return sil;
+            }
+        }
+        return null;
     }
 }
--- LA1/src/model/SongInLibrary.java
+++ LA1/src/model/SongInLibrary.java
@@ -1,18 +1,33 @@
 package model;
 
 public class SongInLibrary {
-	private final Song song;
-	private boolean isFavorite;
-	private int rating;
-	
-	public SongInLibrary(Song s) {
-		song = s;
-		isFavorite = false;
-		rating = 0;
-	}
+    private final Song song;
+    private boolean isFavorite;
+    private int rating;
 
-	public Song getSong() {
-		return song;
+    public SongInLibrary(Song s) {
+        song = s;
+        isFavorite = false;
+        rating = 0;
+    }
 
-	}
+    public Song getSong() {
+        return song;
+    }
+
+    public void rate(int r) {
+        rating = r;
+    }
+
+    public void markFavorite() {
+        isFavorite = true;
+    }
+
+    public void markUnFavorite() {
+        isFavorite = false;
+    }
+
+    public boolean isFavorite() {
+        return isFavorite;
+    }
 }
